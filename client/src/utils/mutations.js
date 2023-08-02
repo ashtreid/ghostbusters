c2d199b8da856fd53e91386ca6b5d8b7b47fae46
Aegean <todddharni@gmail.com>
2023-08-01 19:50:21 -0600

    Added GQL Queries to 'mutations.js' and 'queries.js'

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,41 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_PIN = gql`
+  mutation addPin($pinText: String!) {
+    addPin(pinText: $pinText) {
+      _id
+      pinText
+      pinAuthor
+      createdAt
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($pinId: ID!, $commentText: String!) {
+    addComment(pintId: $pinId, commentText: $commentText) {
+      _id
+      pinText
+      pinAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
