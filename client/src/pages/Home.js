2767e670aacf87e43752b9924a4118ae1b1d2c21
Ash Reid <ashley.reid013@gmail.com>
2023-08-08 16:44:47 -0600

    adds video and carousel to about page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Container, Row, Col, Stack, Carousel, Image, Figure, Anchor } from 'react-bootstrap';
+import telephone from '../images/telephone.gif';
+import people from '../images/people.png';
+import paranoid from '../images/paranoid.png';
+import basement from '../images/basement.png';
 
 // import { useQuery } from '@apollo/client';
 
@@ -7,31 +12,85 @@ import React from 'react';
 
 // import { QUERY_THOUGHTS } from '../utils/queries';
 
+const GhostBustersAd = () => {
+  return (
+    <div className='video-responsive'>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/sRee26pfVzU"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen>
+      </iframe>
+    </div>
+  );
+};
+
 const Home = () => {
 //   const { loading, data } = useQuery(QUERY_THOUGHTS);
 //   const thoughts = data?.thoughts || [];
 
   return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          {/* <ThoughtForm /> */}
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {/* {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )} */}
-        </div>
-      </div>
-    </main>
+    <Container className='ad'>
+      <Row>
+        <Carousel className='text-center p-4' indicators={false} controls={false}>
+          <Carousel.Item>
+            <p>Are you troubled by strange noises in the middle of the night?</p>
+            <a href='https://www.flaticon.com/free-icons/unease' target='_blank' rel='noreferrer'>
+            <Figure.Image
+              width={100}
+              height={100}
+              src={paranoid}
+              alt='unease icons'
+              title='Unease icons created by Leremy - Flaticon' 
+              />
+              </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <p>Do you experience feelings of dread in your basement or attic?</p>
+            <a href='https://www.flaticon.com/free-icons/peeping' target='_blank' rel='noreferrer'>
+            <Figure.Image
+              width={100}
+              height={100}
+              src={basement}
+              alt='peeping icons'
+              title='Peeping icons created by Leremy - Flatico' 
+              />
+              </a>
+          </Carousel.Item>
+          <Carousel.Item>
+            <p>Have you or any of your family ever seen a spook, specter, or ghost?</p>
+            <a href='https://www.flaticon.com/free-icons/haunt' target='_blank' rel='noreferrer'>
+            <Figure.Image
+              width={100}
+              height={100}
+              src={people}
+              alt='haunt icons'
+              title="Haunt icons created by Leremy - Flaticon" 
+              />
+              </a>
+          </Carousel.Item>
+          <Carousel.Item >
+            <p>If the answer is yes then don’t wait another minute. Pick up your phone and call the professionals.</p>
+            <a href="https://www.flaticon.com/free-animated-icons/phone" target='_blank' rel='noreferrer'>
+            <Figure.Image
+              width={100}
+              height={100}
+              src={telephone}
+              alt='phone animated icons'
+              title='Phone animated icons created by Freepik - Flaticon' 
+              />
+              </a>
+          </Carousel.Item>
+
+        </Carousel>
+        <Col className='text-center'>
+          <GhostBustersAd />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
