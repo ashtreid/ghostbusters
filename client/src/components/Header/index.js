e6eb31a7dc3988424411bdfc2e4b3001025e9aee
Ash Reid <ashley.reid013@gmail.com>
2023-08-09 18:38:36 -0600

    re-names classNames for styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -41,6 +41,7 @@ const Header = () => {
 
       {/* set modal data up */}
       <Modal
+        className='auth-modal'
         size='lg'
         show={showModal}
         onHide={() => setShowModal(false)}
--- client/src/components/NewPinModal/index.js
+++ client/src/components/NewPinModal/index.js
@@ -17,24 +17,22 @@ function FormModal({ openForm, onClose, onSubmit, formValues, setFormValues, pos
 
     return (
         <div
-            className={`modal ${openForm ? 'is-active' : ''}`}
+            className={`modal pin-modal ${openForm ? 'is-active' : ''}`}
             style={{
                 position: 'absolute',
                 top: `${position.y - 90}px`, 
                 left: `${position.x + 30}px`,
-                // top: `${position.y}px`, 
-                // left: `${position.x}px`,
             }}
             onClick={(e) => {
                 e.stopPropagation(); 
             }}
         >
-            <div className="modal-card" onClick={(e) => e.stopPropagation()}> 
-                <header className="modal-card-head">
-                    <p className="modal-card-title">Add a Pin!</p>
-                    <button className="delete" aria-label="close" onClick={onClose}></button>
+            <div className="modal-card pin-modal-card" onClick={(e) => e.stopPropagation()}> 
+                <header className="modal-card-head pin-modal-card-head">
+                    <p className="modal-card-title pin-modal-card-title">Add a Pin!</p>
+                    <button className="delete pin-close" aria-label="close" onClick={onClose}></button>
                 </header>
-                <section className="modal-card-body">
+                <section className="modal-card-body pin-modal-card-body">
                     <form onSubmit={handleSubmit}>
                         <div className="field">
                             <label className="label">Pin Title</label>
